58d67cc62ad738c4f873bff9634bda8b79edc0df
Dimitri Kadashev <dkadashev@gmail.com>
2026-07-07 20:17:10 +0700

    First iteration on the code: just shows a "window"
    At the moment the app just shows a window and does nothing - has to be
    killed with Ctrl-C.

---
+++ src/gui/wayland.rs
@@ -0,0 +1,297 @@
+// SPDX-License-Identifier:  GPL-3.0-or-later
+use std::time::Duration;
+
+use smithay_client_toolkit::{
+    compositor::{CompositorHandler, CompositorState},
+    delegate_compositor, delegate_layer, delegate_output, delegate_shm, output,
+    reexports::{calloop, calloop_wayland_source},
+    shell::{
+        WaylandSurface,
+        wlr_layer::{
+            Anchor, KeyboardInteractivity, Layer, LayerShell, LayerShellHandler, LayerSurface,
+            LayerSurfaceConfigure,
+        },
+    },
+    shm::{Shm, ShmHandler, slot::SlotPool},
+};
+use wayland_client::{
+    Connection, Dispatch, QueueHandle,
+    globals::{GlobalList, GlobalListContents, registry_queue_init},
+    protocol::{wl_output, wl_registry, wl_shm, wl_surface},
+};
+
+// LATER: replace with something appropriate
+// LATER: change the `expect()`s to error forwarding (using `anyhow`?)
+// LATER: handle keyboard, exit on any key to start with
+// For now this just shows a grey bar at the top of the screen
+pub fn run_test_ui() {
+    // Create a Wayland connection by connecting to the server through the
+    // environment-provided configuration. See https://wayland-book.com/wayland-display.html
+    let conn = Connection::connect_to_env().expect("failed to establish wayland connection");
+
+    // Make an event queue and retrieve the list of Wayland global objects (see
+    // https://wayland-book.com/registry.html)
+    let (globals, event_queue) =
+        registry_queue_init::<GuiState>(&conn).expect("failed to init wayland event queue");
+    let qh = event_queue.handle();
+
+    // This will be processing all wayland events and sending them to us to handle
+    let mut event_loop: calloop::EventLoop<GuiState> =
+        calloop::EventLoop::try_new().expect("failed to initialize an event loop");
+    // WaylandSource is an adaptor for wayland_client::EventQueue to act as a calloop's event source
+    calloop_wayland_source::WaylandSource::new(conn, event_queue)
+        .insert(event_loop.handle())
+        .expect("failed to insert wayland event source into the event loop");
+
+    let mut state = GuiState::new(&globals, &qh);
+
+    println!("Hit Ctrl+C to exit");
+    while !state.done {
+        event_loop
+            .dispatch(Duration::from_secs(1), &mut state)
+            .expect("event loop dispatch failed");
+    }
+}
+
+struct SurfaceGeometry {
+    width: u32,
+    height: u32,
+    scale_factor: u32,
+}
+
+impl Default for SurfaceGeometry {
+    fn default() -> SurfaceGeometry {
+        SurfaceGeometry {
+            width: 512,
+            height: 25,
+            scale_factor: 1,
+        }
+    }
+}
+
+struct GuiState {
+    output_state: output::OutputState,
+    shm: Shm,
+    pool: SlotPool,
+
+    surface_geometry: SurfaceGeometry,
+    surface: LayerSurface,
+
+    done: bool,
+}
+
+impl GuiState {
+    pub fn new(globals: &GlobalList, qh: &QueueHandle<GuiState>) -> GuiState {
+        let shm = Shm::bind(&globals, &qh).expect("wl_shm is not available");
+        // The initial pool size is 1MB, should be enough even for 4K displays, but if it's not then
+        // the pool will be automatically resized
+        let pool = SlotPool::new(1 * 1024 * 1024, &shm).expect("failed to create a mem pool");
+
+        GuiState {
+            output_state: output::OutputState::new(&globals, &qh),
+            shm: shm,
+            pool: pool,
+
+            surface_geometry: Default::default(),
+            surface: prepare_surface(&globals, &qh),
+
+            done: false,
+        }
+    }
+
+    pub fn draw(&mut self, _qh: &QueueHandle<Self>) {
+        let width = self.surface_geometry.width * self.surface_geometry.scale_factor;
+        let height = self.surface_geometry.height * self.surface_geometry.scale_factor;
+
+        let (buffer, canvas) = self
+            .pool
+            .create_buffer(
+                width as i32,
+                height as i32,
+                width as i32 * 4, // stride, 4 bytes per pixel, hence multiply by 4
+                wl_shm::Format::Argb8888,
+            )
+            .expect("failed to get a buffer from the pool");
+
+        for px in canvas.chunks_exact_mut(4) {
+            px.copy_from_slice(&[0xa0, 0xa0, 0xa0, 0xff]);
+        }
+
+        self.surface
+            .wl_surface()
+            .damage_buffer(0, 0, width as i32, height as i32);
+
+        buffer
+            .attach_to(self.surface.wl_surface())
+            .expect("failed to attach buffer to the surface");
+        self.surface.commit();
+    }
+}
+
+// Implement `Dispatch<WlRegistry, GlobalListContents> for our state. Necessary for
+// being able to use registry_queue_init()
+impl Dispatch<wl_registry::WlRegistry, GlobalListContents> for GuiState {
+    fn event(
+        _state: &mut Self,
+        _: &wl_registry::WlRegistry,
+        _: wl_registry::Event,
+        _: &GlobalListContents,
+        _: &Connection,
+        _: &QueueHandle<GuiState>,
+    ) {
+    }
+}
+
+// Necessary for delegate_compositor
+impl CompositorHandler for GuiState {
+    fn scale_factor_changed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        new_factor: i32,
+    ) {
+        self.surface_geometry.scale_factor = new_factor as u32;
+        // The surface is double buffered, the following call affects the pending buffer,
+        // i.e. the next one that will be drawn with draw(), not the currently active one
+        self.surface
+            .set_buffer_scale(self.surface_geometry.scale_factor)
+            .expect("failed to set buffer scale");
+    }
+
+    fn frame(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _surface: &wl_surface::WlSurface,
+        _time: u32,
+    ) {
+        // We do not request frame callbacks (since the only reason to redraw the screen for us is
+        // if the rendered text is changed, which can only happen when user pressed some button), so
+        // we do not do anything even if we received one (but this should not happen)
+    }
+
+    fn transform_changed(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: &wl_surface::WlSurface,
+        _: wl_output::Transform,
+    ) {
+    }
+
+    fn surface_enter(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: &wl_surface::WlSurface,
+        _: &wl_output::WlOutput,
+    ) {
+    }
+
+    fn surface_leave(
+        &mut self,
+        _: &Connection,
+        _: &QueueHandle<Self>,
+        _: &wl_surface::WlSurface,
+        _: &wl_output::WlOutput,
+    ) {
+    }
+}
+
+impl output::OutputHandler for GuiState {
+    fn output_state(&mut self) -> &mut output::OutputState {
+        &mut self.output_state
+    }
+
+    fn new_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+
+    fn update_output(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+
+    fn output_destroyed(
+        &mut self,
+        _conn: &Connection,
+        _qh: &QueueHandle<Self>,
+        _output: wl_output::WlOutput,
+    ) {
+    }
+}
+delegate_compositor!(GuiState);
+
+// Necessary for delegate_shm
+impl ShmHandler for GuiState {
+    fn shm_state(&mut self) -> &mut Shm {
+        &mut self.shm
+    }
+}
+delegate_shm!(GuiState);
+
+// Necessary for delegate_layer
+impl LayerShellHandler for GuiState {
+    fn closed(&mut self, _conn: &Connection, _qh: &QueueHandle<Self>, _layer: &LayerSurface) {
+        self.done = true;
+    }
+
+    fn configure(
+        &mut self,
+        _conn: &Connection,
+        qh: &QueueHandle<Self>,
+        _layer: &LayerSurface,
+        configure: LayerSurfaceConfigure,
+        _serial: u32,
+    ) {
+        if configure.new_size.0 > 0 {
+            self.surface_geometry.width = configure.new_size.0
+        }
+        // we ignore the height hint, our height is fixed
+
+        // Re-draw the shell surface, useful in two cases: initial configure / draw and when
+        // the surface size has changed. The latter should not really be happening, but hopefully we
+        // won't get any/too many `configure` events in that case, so we just re-draw
+        // unconditionally.
+        self.draw(qh);
+    }
+}
+delegate_layer!(GuiState);
+
+delegate_output!(GuiState);
+
+fn prepare_surface(globals: &GlobalList, qh: &QueueHandle<GuiState>) -> LayerSurface {
+    // Bind wl_compositor, the thing that will hand us a surface to use.
+    // See https://wayland-book.com/surfaces/compositor.html
+    let compositor = CompositorState::bind(&globals, &qh).expect("wl_compositor is not available");
+    // Bind zwlr_layer_shell (rather than xdg_shell), since our window is supposed to be on top of
+    // everything and capturing all input, rather than a normal desktop window. See
+    // https://docs.rs/smithay-client-toolkit/latest/smithay_client_toolkit/shell/index.html
+    let layer_shell = LayerShell::bind(&globals, &qh).expect("zwlr_layer_shell is not available");
+
+    let surface = layer_shell.create_layer_surface(
+        &qh,
+        compositor.create_surface(&qh),
+        Layer::Top,
+        Some("wlprompt"),
+        None,
+    );
+
+    surface.set_anchor(Anchor::LEFT | Anchor::TOP | Anchor::RIGHT);
+    // LATER: we actually want Exclusive interactivity to catch all input, but for now keyboard
+    // input is not supported, so reflect that
+    surface.set_keyboard_interactivity(KeyboardInteractivity::None);
+    // See https://wayland.app/protocols/wlr-layer-shell-unstable-v1#zwlr_layer_surface_v1:request:set_exclusive_zone
+    surface.set_exclusive_zone(-1);
+    surface.commit();
+
+    surface
+}
---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier:  GPL-3.0-or-later
+mod gui;
+
+pub fn run() {
+    gui::wayland::run_test_ui();
+}
